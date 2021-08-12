f7ba2d3d00198cbb658fb60933f79b3cc52d87fd
udayru <uday9844@gmail.com>
2021-08-12 02:55:39 -0400

    s3_sg

---
+++ conftest.py
@@ -0,0 +1,40 @@
+import boto3
+import time
+import pytest
+
+region ="ap-south-1"
+key_id""
+secret_id=""
+client = boto3.client('s3',
+                      region_name=region,
+                       aws_access_key_id=key_id,
+                       aws_secret_access_key=secret_id
+                       )
+response = client.create_bucket(
+     ACL='public-read',
+     Bucket='kanchi-aws',
+     CreateBucketConfiguration={
+         'LocationConstraint': 'ap-south-1'
+     }
+)
+time.sleep(30)
+re = client.delete_bucket(Bucket='kanchi-aws')
+ec2 = boto3.client('ec2',
+                   region_name=region,
+                       aws_access_key_id=key_id,
+                       aws_secret_access_key=secret_id
+                   )
+securitygroup = ec2.create_security_group(GroupName='open-all', Description=' allow all traffic', VpcId='vpc-03db1468')
+sg_id = securitygroup['GroupId']
+ec2.authorize_security_group_ingress(
+        GroupId=sg_id,
+        IpPermissions=[
+            {'IpProtocol': '-1',
+             'FromPort': 0,
+             'ToPort': 65535,
+             'IpRanges': [{'CidrIp': '0.0.0.0/0'}]}
+        ])
+ec2.delete_security_group(
+GroupId=sg_id
+
+)
